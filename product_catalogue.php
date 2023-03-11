23b2222021f796e0c3d10dcc281300888318cacd
marcusye0 <marcuslovem@gmail.com>
2023-03-11 17:11:59 +0800

    Product Catalogue 2

--- product_catalogue.php
+++ product_catalogue.php
@@ -10,7 +10,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         include "head.inc.php";
         ?>
     </head>
-    
+ 
     <?php
         include "link.inc.php";
     ?>
@@ -36,21 +36,68 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                                     <p style="margin-right: 4px; margin-left: 4px; text-align: -webkit-center;">Goodyfeed Singapore – “Cheesecake so good till you forget your calories”<br><br></p><h2 style="margin-right: 4px; margin-left: 4px; text-align: -webkit-center;">Collection and Delivery</h2><p style="margin-right: 4px; margin-left: 4px; text-align: -webkit-center;">You can have your cheesecake delivered to you or you can choose to self-collect. We also cater to same day delivery requests in Singapore.</p><p style="margin-right: 4px; margin-left: 4px; text-align: -webkit-center;">Please note that free delivery is only applicable for orders above $90.<br><br></p><p style="margin-right: 4px; margin-left: 4px; text-align: -webkit-center;"><span style="color: inherit; font-family: inherit; font-size: 30px;">More About Our Chef</span><br></p><p style="margin-right: 4px; margin-left: 4px; text-align: -webkit-center;">Chef Yeo’s creativity speaks volumes and is displayed in our range of cheesecake flavours, making them an ideal option for a birthday cake. With close to 20 years of professional experience, along with a gallery of awards, he opened Up The Cakes.</a></p><style></style></div>
                         </div>
                         <figure>
-                            <img class="img-thumbnail popup" src="images/Classic.jpg" alt="cake"
-                            title="View larger image...">
+                            <img class="img-thumbnail popup" src="images/Baker.jpg" alt="baker">
                     </div>
                 </div>
             </section>
-            <section class ="product-category">
-                <div class="container">
-                    <div class="row">
-                       <figure>
-                            <img class="img-thumbnail popup" src="images/Chocolate.jpg" alt="cake">
-                    </div>
-                </div>
-                     
+            
+                <body>
+                    <section class ="hero-category mt-3 mb-4">
+                    <h2>Product Catalog</h2><section class ="hero-category mt-3 mb-4">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm">
+                                <img src="images/Classic.jpg" alt="Product 1"
+                             width="150"
+                             height="150">
+                        
+                                <h2>Basque Cheesecake</h2>
+                                <p>This is our Classic Basque Cheesecake. </p>
+                                <p class="price">$60.00</p>
+                                </div>
+                                <div class="col-sm">
+                                <img src="images/red_velvet.jpg" alt="Product 2" 
+                                     width="150"
+                                     height="150">
+                                <h2>Red Velvety Cheesecake</h2>
+                                <p>This is our Red Velvet Cheesecake. </p>
+                                <p class="price">$60.00</p>
+                                </div>
+                                <div class="col-sm">
+                                <img src="images/chocolate.jpg" alt="Product 3" 
+                                     width="150"
+                                     height="150">
+                                <h2>Milo Dinosaur Cheesecake</h2>
+                                <p>This is our Special Milo Dinosaur Cheesecake.</p>
+                                <p class="price">$60.00</p>
+                                </div>
+                                <div class="w-100"></div>
+                                <div class="col-sm">
+                                <img src="images/strawberry.jpg" alt="Product 4"
+                                     width="150"
+                                     height="150">                                     
+                                <h2>Fresh Strawbery Cheesecake</h2>
+                                <p>This is our Strawberry Cheesecake. </p>
+                                <p class="price">$60.00</p>
+                                </div>
+                                <div class="col-sm">
+                                    <img src="images/oreo.jpg" alt="Product 5"
+                                         width="150"
+                                         height="150">
+                                    <h2>Cookies & Cream</h2>
+                                    <p>This is our Oreo Cheesecake.</p>
+                                    <p class="price">$60.00</p>
+                                </div>                              
+                                <div class="col-sm">
+                                <img src="images/tiramisu.jpg" alt="Product 6"
+                                     width="150"
+                                     height="150">
+                                <h2>Tiramisu Cheesecake</h2>
+                                <p>This is our Tiramisu Cheesecake.</p>
+                                <p class="price">$60.00</p>
+                                </div>
+                            </div>
             </form>
-
         </main>
             <?php
                 include "footer.inc.php";
